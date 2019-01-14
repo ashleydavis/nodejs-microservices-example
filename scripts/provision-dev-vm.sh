f91416df7036cfa82ee0eb77d8379843365d83b1
Ashley Davis <ashley@codecapers.com.au>
2019-01-14 14:56:53 +1000

    Now installing Kubectl into dev env.

--- scripts/provision-dev-vm.sh
+++ scripts/provision-dev-vm.sh
@@ -58,3 +58,12 @@ terraform --version
 #
 sudo groupadd docker
 sudo usermod -aG docker $USER
+
+#
+# Install Kubectl
+#
+sudo apt-get update && sudo apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get -yq update
+sudo apt-get install -yq kubectl
