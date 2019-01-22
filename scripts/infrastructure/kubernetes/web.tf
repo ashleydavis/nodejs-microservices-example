b31d8c430283ae4d351fef2114af152e0b04bf2e
Ashley Davis <ashley@codecapers.com.au>
2019-01-22 13:20:03 +1000

    Separated each microservice out to own Terraform configuration file.

---
+++ scripts/infrastructure/kubernetes/web.tf
@@ -0,0 +1,59 @@
+resource "kubernetes_deployment" "web" {
+  metadata {
+    name = "web"
+
+    labels {
+      test = "web"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels {
+        test = "web"
+      }
+    }
+
+    template {
+      metadata {
+        labels {
+          test = "web"
+        }
+      }
+
+      spec {
+        container {
+          image = "${var.docker_registry_name}.azurecr.io/web:${var.version}"
+          name  = "web"
+
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "web" {
+  metadata {
+    name = "web"
+  }
+
+  spec {
+    selector {
+      test = "${kubernetes_deployment.web.metadata.0.labels.test}"
+    }
+
+    session_affinity = "ClientIP"
+
+    port {
+      port        = 80
+      target_port = 80
+    }
+
+    type = "LoadBalancer"
+  }
+}
