64d0fbe745ab10dbd981b67e36622fee1d7eda8b
Ashley Davis <ashley@codecapers.com.au>
2019-01-22 13:57:21 +1000

    Now creating the Azure storage container to persist Terraform state.

--- scripts/infrastructure/docker/docker-registry.tf
+++ scripts/infrastructure/docker/docker-registry.tf
@@ -13,6 +13,13 @@ resource "azurerm_storage_account" "docker_storage" {
   account_replication_type = "GRS"
 }
 
+resource "azurerm_storage_container" "terraform_container" {
+  name                  = "terraform"
+  resource_group_name   = "${azurerm_resource_group.main.name}"
+  storage_account_name  = "${azurerm_storage_account.docker_storage.name}"
+  container_access_type = "private"
+}
+
 resource "azurerm_container_registry" "docker_registry" {
   name                = "${var.docker_registry_name}"
   resource_group_name = "${azurerm_resource_group.main.name}"
